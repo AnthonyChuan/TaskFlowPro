0f76cc334155df60800bcb40674a39a1a9615cf8
Anthony <anthonychuan1@gmail.com>
2026-03-27 16:48:48 -0500

    cambios en pagina de inicio

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <p className="text-2xl text-center">
-        La ruta para empezar a usar el sistema es <strong>/login</strong>
+        Para usar el sistema
       </p>
+      
+      <Link href={"/login"}>
+        <Button>presiona aqui</Button>
+        </Link>
+        
     </div>
   );
 }
